f5951890de1848946b0f0bf44394107793b58f49
beh0907 <qkrdlsguq@naver.com>
2023-04-15 11:33:55 +0900

    ver02

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -1,79 +1,79 @@
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react'
-import { Row, Col, Button, Form, FormControl } from 'react-bootstrap'
+import { Row, Col, Button, Form, FormControl, Card, Image } from 'react-bootstrap'
+import Book from './Book';
 
 const BookPage = () => {
-    const bookRef = useRef(null);
-    const [loading, setLoading] = useState(false);
     const [books, setBooks] = useState([])
-    const [query, setQuery] = useState('리액트')
-    const [page, setPage] = useState(1)
-    const [end, setEnd] = useState(false)
-    const [total, setTotal] = useState(0);
+    const [query, setQuery] = useState("안드로이드")
+    const [page, setPage] = useState(1햐)
+    const [loading, setLoading] = useState(false)
+    const [isEnd, setIsEnd] = useState(false)
+    const [total, setTotal] = useState(0)
 
-    const getBooks = async () => {
+    const getdata = async () => {
         const url = "https://dapi.kakao.com/v3/search/book?target=title";
         const config = {
             headers: { "Authorization": 'KakaoAK 7d083064dcb5ec4a4a7ddbbf9b1025c5' },
-            params: { "query": query, "size": 6, "page": page }
+            params: { "query": query, "size": 8, "page": page }
         }
 
-        setLoading(true)
-        const result = await axios.get(url, config)
-        console.log(result)
+        const json = await axios.get(url, config);
+        console.log(json)
 
-        setTotal(result.data.meta.pageable_count)
-        setEnd(result.data.meta.is_end)
-        setBooks(result.data.documents);
+        setBooks(json.data.documents)
+        setTotal(json.data.meta.pageable_count)
+        setIsEnd(json.data.meta.is_end)
         setLoading(false)
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = e => {
         e.preventDefault()
         setPage(1)
-        getBooks()
-        
-        bookRef.current.focus()
+        getdata()
     }
 
     useEffect(() => {
-        getBooks();
-    }, [page]);
+        setLoading(true)
+        getdata()
+    }, [page])
 
     if (loading) return <h1 className='text-center my-5'>로딩중......</h1>
-    return (
-        <Row className='my-5 mx-2'>
-            <Col xs={6} md={2}>
-                <Form onSubmit={onSubmit}>
-                    <FormControl value={query} onChange={(e) => setQuery(e.target.value)} placeholder='검색어' ref={bookRef} />
-                </Form>
+    else
+        return (
+            <Row>
+                <h1 className='text-center my-5'>도서검색</h1>
 
-                <div className='my-3'>검색 수 : {total} 건</div>
-            </Col>
-            <hr />
-            <Col>
-                <h1 className='text-center'>도서검색</h1>
+                <Row>
+                    <Col md={3}>
+                        <Form onSubmit={onSubmit}>
+                            <Form.Control value={query} onChange={e => setQuery(e.target.value)} placeholder='검색어'></Form.Control>
+                        </Form>
+                        검색 건 : {total}
+                    </Col>
+                </Row>
                 <Row>
                     {books.map(book =>
-                        <Col key={book.isbn} className='box m-2'>
-                            <img src={book.thumbnail ? book.thumbnail : 'http://via.placeholder.com/170x150'} />
-                            <div className='ellipsis'>{book.title}</div>
-                            <div className='ellipsis'>{book.price} 원</div>
+                        <Col key={book.isbn} md={3} xs={6} className='my-3'>
+                            <Card>
+                                <Card.Body>
+                                    <Image src={book.thumbnail ? book.thumbnail : 'http://via.placeholder.com/170x150'} />
+                                    <div className='ellipsis'>{book.title}</div>
+                                    <div className='ellipsis'>{book.price} 원</div>
+                                    <Book book={book} />
+                                </Card.Body>
+                            </Card>
                         </Col>
-
                     )}
+
+                    <Col className='text-center my-3'>
+                        <Button onClick={() => setPage(page - 1)} disabled={page === 1}>이전</Button>
+                        <span className='mx-3'>{page}</span>
+                        <Button onClick={() => setPage(page + 1)} disabled={isEnd}>다음</Button>
+                    </Col>
                 </Row>
-
-                <div className='text-center my-3'>
-                    <Button onClick={() => setPage(page - 1)} disabled={page === 1}>이전</Button>
-                    <span className='mx-3'>{page}</span>
-                    <Button onClick={() => setPage(page + 1)} disabled={end}>다음</Button>
-                </div>
-
-
-            </Col>
-        </Row>
-    )
+            </Row >
+        )
 }
 
 export default BookPage
